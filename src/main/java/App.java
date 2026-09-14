a5af62791ad28e6a8b5286401b057ca11f419b56
alskill <fiazefaq@gmail.com>
2026-09-14 12:27:09 +0530

    Add Maven Java application

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+```java
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Maven Project!");
+        System.out.println("Build successful!");
+    }
+}
+```
