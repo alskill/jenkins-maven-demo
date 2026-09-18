77160560ff9d39f23c3eb3a8b120ddc826e42443
alskill <fiazefaq@gmail.com>
2026-09-18 12:24:11 +0530

    file added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Maven Project!");
         System.out.println("Build successful!");
+        System.out.println("Hello Jenkins CI/CD!");
     }
 }
